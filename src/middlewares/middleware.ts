411a77cb3ef9fd1bd60b08b5eedb38a0e226079d
Kim Donghyun <kimdonghyun@Kimui-MacBookAir.local>
2023-02-11 22:04:56 +0900

    [ADDED] middleware

---
+++ src/middlewares/middleware.ts
@@ -0,0 +1,14 @@
+import { Next } from 'koa';
+import { DefaultCtx } from 'types/request.types';
+import { Logger } from 'utilities/logger.utils';
+
+export const middleware = async (ctx: DefaultCtx, next: Next) => {
+  try {
+    const { clientid } = ctx;
+    Logger.info('Received ClientId from Context: %o', { clientid });
+
+    await next();
+  } catch (error: unknown) {
+    throw new Error(`Received Middleware Error: ${error}`);
+  }
+};
