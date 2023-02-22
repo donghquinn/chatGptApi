becf029af9c7852ba93037c2ae6245275cda6fbe
Kim, Dong H <ehdgus1524@gmail.com>
2023-02-22 14:38:54 +0900

    added: SetError Message

--- src/dto/response.dto.ts
+++ src/dto/response.dto.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { KeyableObject, ResponseBody } from 'types/request.types';
+import { ZodError } from 'zod';
 
 @Injectable()
 export class SetResponse implements ResponseBody {
@@ -10,5 +11,21 @@ export class SetResponse implements ResponseBody {
   }
   resCode: string;
   dataRes: KeyableObject | null;
+  errMsg: string[];
+}
+
+@Injectable()
+export class SetErrorResponse implements ResponseBody {
+  constructor(resCode: number, error: unknown) {
+    const errorMessgaeArray = [];
+    if (error instanceof ZodError) {
+      errorMessgaeArray.push(error.name, error.message);
+    }
+    this.resCode = resCode.toString();
+    this.dataRes = null;
+    this.errMsg = errorMessgaeArray;
+  }
+  resCode: string;
+  dataRes: null;
   errMsg: string[];
 }
