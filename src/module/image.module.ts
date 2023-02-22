6dff840f6f94f2efca1c503aa57e4bdad70b1a62
Kim, Dong H <ehdgus1524@gmail.com>
2023-02-22 22:49:50 +0900

    added: middlewasre ip

--- src/module/image.module.ts
+++ src/module/image.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { GenerateImageController } from 'controllers/img/generate';
 import { GenerateImage } from 'libraries/img/getImages.lib';
 import { PrismaLibrary } from 'libraries/img/prisma/prisma.lib';
 import { PrismaModule } from './prisma.module';
+import { IpMiddleware } from 'middlewares/ip.middlewares';
 
 @Module({
   imports: [PrismaModule],
