b06b466a09d3272fab8b61309bbdc1c54aa16297
Kim, Dong H <ehdgus1524@gmail.com>
2023-03-06 22:16:14 +0900

    wip

--- src/controllers/audio/translation.ctl.ts
+++ src/controllers/audio/translation.ctl.ts
@@ -1,16 +1,22 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { SetErrorResponse } from 'dto/response.dto';
+import { SetErrorResponse, SetResponse } from 'dto/response.dto';
 import { Request } from 'koa';
 import { GenerateAudio } from 'libraries/audio/generate.lib';
+import { TranslateRequestTypes } from 'types/request.types';
+import { translateValidator } from 'validators/request.validator';
 
 @Controller('audio')
 export class AudioController {
   constructor(private readonly translator: GenerateAudio) {}
 
   @Post('/translate')
-  async generateTranslate(@Body() request: Request) {
+  async generateTranslate(@Body() request: TranslateRequestTypes) {
     try {
-      await this.translator.transaltion();
+      const { file, prompt, response_format } = await translateValidator(request);
+
+      const result = await this.translator.transaltion(file, prompt, response_format);
+
+      return new SetResponse(200, { result });
     } catch (error) {
       return new SetErrorResponse(500, error);
     }
