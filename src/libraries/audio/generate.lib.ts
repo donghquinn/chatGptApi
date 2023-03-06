6a0687e1ca67addf8fdc3e44cbb088083536e4bc
Kim, Dong H <ehdgus1524@gmail.com>
2023-03-06 21:56:18 +0900

    fix

--- src/libraries/audio/generate.lib.ts
+++ src/libraries/audio/generate.lib.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { AudioError } from 'error/audio.error';
 import { PrismaLibrary } from 'libraries/common/prisma/prisma.lib';
+import { tokenAndUrlValidator } from 'validators/generate.validator';
 
 @Injectable()
 export class GenerateAudio {
   constructor(private prisma: PrismaLibrary) {}
 
-  async generate() {}
+  async transaltion(file: string, prompt?: string) {
+    try {
+      const url = process.env.CHATGPT_URL!;
+      const token = process.env.CHATGPT_API_TOKEN!;
+
+      const { token: validatedToken, url: validatedUrl } = await tokenAndUrlValidator(token, url);
+
+      const headers = {
+        'content-type': 'application/json',
+        authorization: `Bearer ${validatedToken}`,
+      };
+    } catch (error) {
+      throw new AudioError(
+        'Audio Translation',
+        'Audio Error',
+        error instanceof Error ? error : new Error(JSON.stringify(error)),
+      );
+    }
+  }
 }
